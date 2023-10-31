7a6644f706023698db7df3449ea793cc404b4605
Keshav <keshav0799@gmail.com>
2023-10-31 19:50:00 +0400

    Create Footer component

--- front-end/src/components/home.jsx
+++ front-end/src/components/home.jsx
@@ -2,6 +2,7 @@
 import Banner from "./banner";
 import NavBar from "./navbar";
 import ProductDropScreen from "./productDrop";
+import Footer from "./footer";
 
 import '../index.css'
 
@@ -17,30 +18,10 @@ export default function Home(){
 
         <ProductDropScreen/>
 
-
+        <Footer/>
        
-
-        <div className="footerBox">
-            <div className="shop">
-                <p id="footerTitle"><b>Shop</b></p>
-                <p>About us</p>
-                <p> Gift Cards</p>
-                <p>Contact Us</p>
-                
-            </div>
-            <div className="helpAndAdvice">
-                <p id="footerTitle"><b>Help & Advice </b></p>
-                <p> Shipping & Returns</p>
-                <p>Cookie Policy</p>
-                <p>Privacy Policy</p>
-            </div>
-
-        </div>
-
-
         </main>
       
-
         </>
     )
 }
