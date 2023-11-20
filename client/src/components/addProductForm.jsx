b1eb200ec3032cde0f8418eb68a92e6a35adc6c8
Keshav <keshav0799@gmail.com>
2023-11-20 19:50:36 +0400

    Further style addProductForm so that all label are aligned

--- client/src/components/addProductForm.jsx
+++ client/src/components/addProductForm.jsx
@@ -5,7 +5,9 @@ export default function AddProductForm(){
     const imagesId = useId();
     const nameId = useId();
     const priceId = useId();
-    const categoryId = useId();
+    const equipmentcategoryId = useId();
+    const apparelcategoryId = useId();
+    const accessoriescategoryId = useId();
     const commentsId = useId();
     const featuresId = useId();
 
@@ -16,72 +18,79 @@ export default function AddProductForm(){
         <div className="NewProductSection">
             <div className="NewProductBox">
 
-                <h1>Add new product form</h1>
-                <form className="NewProductForm">
-
-                
-                <div className="addProductField">
-                    <label htmlFor={nameId}>Product Name</label>
-                    <input type="text" id={nameId}/>
-                </div>
-
-                <div className="addProductField">
-                    <label htmlFor={priceId}>Product Price</label>
-                    <input type="text" id={priceId}/>
-                </div>
-
-                <div className="addProductField">
-                     <label >Product Category</label>
-
-                     <div className="radioBtns">
-
-                    
-                     <div className="radioBtn">
-                        <label htmlFor="Apparel" >Apparel</label>
-                        <input type="radio" id= "Apparel" name="Apparel" value="Apparel"/>
-                     </div>
-
-                     <div className="radioBtn">
-                        <label htmlFor="Accessories" >Accessories</label>
-                        <input type="radio" id= "Accessories" name="Accessories" value="Accessories"/>
-                     </div>
-
-
-                     <div className="radioBtn">
-                        <label htmlFor="Equipment" >Equipment</label>
-                        <input type="radio" id= "Equipment" name="Equipment" value="Equipment"/>
-                    </div>   
-
-                        
-                     </div>
-
-
-                    
-                    
-                   
-                </div>
-
-                <div className="addProductField">
-                    <label htmlFor={featuresId}>New Features</label>
-                    <input type="text" id={featuresId}/>
-                </div>
-
-                <div className="addProductField comment ">
-                    <label htmlFor={commentsId}>Comments</label>
-                    <input type="textarea" id={commentsId}/>
-
-                </div>
-
-
-                <div className="addProductField">
-                    <label htmlFor={imagesId}>Product Images</label>
-                    <input type="file" id={imagesId}/>
-
-                </div>
-
             
 
-                <button type="submit">Add</button>
+                <h1>New product form</h1>
+                <form className="NewProductForm">
+
+                    <div className="formField">
+                        <label htmlFor={nameId}>Product Name</label>
+                        <input type="text" id={nameId}/>
+                    </div>
+                   
+
+
+                    <div className="formField">
+                        <label htmlFor={priceId}>Product Price</label>
+                        <input type="text" id={priceId}/>
+                    </div>
+                  
+
+
+                    <div className="formField">
+                    <div className="inline">
+                    <label >Product Category</label>
+                    
+                    <div className="radioBtns">
+
+                     {/* ALl radiobtn input have the same name so that only one can be checked at a time*/}
+                                    
+                    <div className="radioBtn">
+                    <label htmlFor={apparelcategoryId} >Apparel</label>
+                    <input type="radio" id= {apparelcategoryId} name="category" value="Apparel"/>
+                    </div>
+
+                    <div className="radioBtn">
+                    <label htmlFor={accessoriescategoryId} >Accessories</label>
+                    <input type="radio" id= {accessoriescategoryId} name="category" value="Accessories"/>
+                    </div>
+
+
+                    <div className="radioBtn">
+                    <label htmlFor={equipmentcategoryId} >Equipment</label>
+                    <input type="radio" id= {equipmentcategoryId} name="category" value="Equipment"/>
+                    </div>   
+
+
+                    </div>
+
+                    </div>
+                    </div>
+                    
+
+                    
+
+                        <div className="formField">
+                            <label htmlFor={featuresId}>New Features</label>
+                            <textarea id={featuresId}/>  
+
+                        </div>
+                   
+
+                    <div className="formField">
+                        <label htmlFor={commentsId}>Comments</label>
+                        <textarea id={commentsId}/>
+
+                    </div>
+              
+
+                    <div className="formField">
+                        <label htmlFor={imagesId}>Product Images</label>
+                        <input type="file" id={imagesId}/>
+                    </div>
+                  
+                
+                <button type="submit"  className="addProductBtn">Add Product</button>
 
 
                 </form>
