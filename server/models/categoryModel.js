d1bdd3478f5374fca15ec0bbe936f519918f16bd
Keshav <keshav0799@gmail.com>
2023-11-23 12:25:19 +0400

    Add trim and lowercase validation to productModel

--- server/models/categoryModel.js
+++ server/models/categoryModel.js
@@ -4,11 +4,14 @@ const categorySchema = new mongoose.Schema({
     name: {
     type:String,
     required:true,
+    
+
     },
 
     description: {
         type:String,
         required:true,
+
     },
     
 });
--- server/models/productModel.js
+++ server/models/productModel.js
@@ -4,6 +4,8 @@ const productSchema = new mongoose.Schema({
     name: {
     type:String,
     required:true,
+    trim:true,
+    lowercase:true,
     },
 
     comments: {
