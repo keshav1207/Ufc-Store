779b78ee7ac192218a4221dbe0f52906123c0a08
Keshav <keshav0799@gmail.com>
2023-11-28 21:36:24 +0400

    Add optionalfilter param to getCategoryProducts axios request

--- client/src/apicalls/getCategoryProducts.js
+++ client/src/apicalls/getCategoryProducts.js
@@ -6,10 +6,10 @@ import { axiosMultiFormInstance } from "./axiosMultiFormInstance";
 
 //Get all products from a category
 
-export const getCategoryProducts = async function(category){
+export const getCategoryProducts = async function(category,optionalFilter){
     try {
         
-        const response = await axiosMultiFormInstance.get(`http://localhost:5000/api/categories/${category}`);
+        const response = await axiosMultiFormInstance.get(`http://localhost:5000/api/categories/${category}`,{params:{optionalFilter}});
         return response.data;
 
     } catch (error) {
