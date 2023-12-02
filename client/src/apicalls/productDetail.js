939cc32b0dd2e532f902781be5c38762ac739329
Keshav <keshav0799@gmail.com>
2023-12-02 12:43:51 +0400

    Create deleteProduct axios delete request

--- client/src/apicalls/productDetail.js
+++ client/src/apicalls/productDetail.js
@@ -2,8 +2,23 @@ import axios from 'axios';
 
 export const ProductDetail = async function(productId){
     try {
-        console.log("Axios activated");
+       
         const response = await axios.get(`http://localhost:5000/api/product/${productId}`);
+        
+        return response.data;
+        
+    } catch (error) {
+        if(error.response){
+            return `${error.response.data.msg}`
+        } 
+    }
+}
+
+
+export const DeleteProduct = async function(productId){
+    try {
+        
+        const response = await axios.delete(`http://localhost:5000/api/product/${productId}`);
         
         return response.data;
         
--- client/src/components/productList.jsx
+++ client/src/components/productList.jsx
@@ -1,18 +1,67 @@
-
+import { useEffect,useState} from "react"
+import { getAllProducts } from "../apicalls/getAllProducts"
 
 
 export default function ProductList(){
+
+    //Create a state to hold the productInfo fetched from database
+    const [productInfo, setProductInfo] = useState(null);
+
+
+    useEffect(()=> {
+    const fetchData = async()=>{
+        try {
+            const response = await getAllProducts();
+            setProductInfo(response.data);
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+    
+    fetchData();
+        
+    },[])
+
+
+
     return(
         <>
         <div className="productSection">
+        
+        <div className="manageProductHeading">Manage Products Page</div>
 
         <button className="addProductBtn">Add New Product</button>
 
         <div className="products">
-                
+
+        <div className="productLine">
+
+        <div><b>Images</b></div>
+        <div><b>Name</b></div>
+        <div><b>Price</b></div>
 
         </div>
-        
+
+                {productInfo?(productInfo.map((item,index)=>(
+
+                        <div className="productLine" key={index}>
+
+                        <img src={item.images[0]}/>
+                        <div>{item.name}</div>
+                        <div>${item.price}</div>
+                        <div className="buttons">
+                        <button className="editBtn">Edit</button>
+                        <button className="deleteBtn">Delete</button>
+                        </div>
+                        
+                        </div>
+                        
+                          
+                ))):(null)}
+
+        </div>
+
         </div>
         
       
