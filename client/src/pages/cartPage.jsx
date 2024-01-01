02abf372cbc9bd277e6b9e9a6667d0b57f90e6b2
Keshav <keshav0799@gmail.com>
2024-01-01 12:36:49 +0400

    Create CartPage

---
+++ client/src/pages/cartPage.jsx
@@ -0,0 +1,25 @@
+import Banner from '../components/banner'
+import Footer from '../components/footer'
+import NavBar from '../components/navbar'
+import CartContent from '../components/cartContent'
+
+
+export default function CartPage(){
+    return(
+        <>
+        <main>
+        <Banner/>
+        <NavBar/>
+       <CartContent/>
+        <Footer/>
+        </main>
+        
+        
+        </>
+
+
+
+
+
+    )
+}
