ab558ad1b595d92d5ccd4fa32c47a136331a3766
Keshav <keshav0799@gmail.com>
2024-01-07 20:20:51 +0400

    Add loading Spinner to homepage

--- client/src/components/productDrop.jsx
+++ client/src/components/productDrop.jsx
@@ -1,21 +1,25 @@
 
-import '../index.css'
 import { getNewProducts } from "../apicalls/getNewProducts"
 import { useEffect, useState } from "react"
 import { Link } from 'react-router-dom';
+import LoadingSpinner from "./loadingSpinner";
 
 
 export default function ProductDropScreen(){
+    const [isLoading, setIsLoading] = useState(false);
     const[data,setData] = useState(null);
 
     useEffect(()=>{
         const fetchData =  async ()=>{
             try {
+                setIsLoading(true);
                 const response = await getNewProducts();
                 setData(response.data);
+                setIsLoading(false);
                
             } catch (error) {
                 console.log(error);
+                setIsLoading(false);
             }
            
         };
@@ -31,33 +35,35 @@ return(
 
     <p>NEW PRODUCT DROPS</p>
 
-    <div className="productBox">
+    {isLoading? <LoadingSpinner/>:( <div className="productBox">
 
-    {data?(data.map((item,index)=>(
+{data?(data.map((item,index)=>(
 
 <Link to={`/products/${item._id}`}   key={item._id}>
-    <div className="product">
-        <div className="picture">
-    <img src= {item.images[0]} alt="" />
+<div className="product">
+    <div className="picture">
+<img src= {item.images[0]} alt="" />
 </div>
 
 <div className="info" >
-    
-    <div className="productName">{item.name.toUpperCase().substring(0,30)}  </div>
-    <div className="productPrice">${item.price}</div>
+
+<div className="productName">{item.name.toUpperCase().substring(0,30)}  </div>
+<div className="productPrice">${item.price}</div>
 </div>
-    </div>
+</div>
 
 </Link>
-       
+   
 
-))):(<p>Loading...</p>)
+))):(<LoadingSpinner/>)
 
 
 }
-    
 
-    </div>
+
+</div>)}
+
+   
     </div>
     
     </>
