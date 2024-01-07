a890e32128c2d08eed2cc3b0e139a2378ec05c2e
Keshav <keshav0799@gmail.com>
2024-01-07 20:28:13 +0400

    Add Loading spinner to productsGrid on category page

--- client/src/components/productsGrid.jsx
+++ client/src/components/productsGrid.jsx
@@ -3,10 +3,14 @@ import { getCategoryProducts } from "../apicalls/getCategoryProducts"
 import { useEffect, useState } from "react";
 import { useSelector} from 'react-redux'
 import { Link } from 'react-router-dom';
+import LoadingSpinner from './loadingSpinner';
 
 export default function ProductsGrid(props){
+
+    const [isLoading, setIsLoading] = useState(false);
     
     const filter = useSelector((state) => state.filter.filter);
+
    
 
         const[myarray,setArray] = useState(null);
@@ -17,10 +21,13 @@ export default function ProductsGrid(props){
         useEffect(()=>{
             const fetchData =  async ()=>{
                 try {
+                    setIsLoading(true);
                     const response = await getCategoryProducts(props.categorySelected,filter);
                     setArray(response.data);
+                    setIsLoading(false);
                 } catch (error) {
                     console.log(error);
+                    setIsLoading(false);
                 }
                
             };
@@ -38,38 +45,38 @@ export default function ProductsGrid(props){
     
     return(
         <>
-
-        < div className="ProductsGrid">
+    {isLoading? <LoadingSpinner/>:( < div className="ProductsGrid">
 
 
        
-                {myarray?(myarray.map((item,index)=>(
+        {myarray?(myarray.map((item,index)=>(
 
-                <Link to={`/products/${item._id}`}   key={item._id}>
-                    <div className="product">
-                        <div className="picture">
-                    <img src= {item.images[0]} alt="" />
-                </div>
-
-                <div className="info" >
-                    
-                    <div className="productName">{item.name.toUpperCase().substring(0,30)}  </div>
-                    <div className="productPrice">${item.price}</div>
-                </div>
-                    </div>
-                
-                </Link>
-                       
-
-                ))):(<p>Loading...</p>)
-                
-                
-                }
-                
+        <Link to={`/products/${item._id}`}   key={item._id}>
+            <div className="product">
+                <div className="picture">
+            <img src= {item.images[0]} alt="" />
         </div>
 
-        
-        
+        <div className="info" >
+            
+            <div className="productName">{item.name.toUpperCase().substring(0,30)}  </div>
+            <div className="productPrice">${item.price}</div>
+        </div>
+            </div>
+
+        </Link>
+            
+
+        ))):(<LoadingSpinner/>)
+
+
+        }
+
+        </div>
+        )}
+            
+                
+                
         </>
     )
 }
