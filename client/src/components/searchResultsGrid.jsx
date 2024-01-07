fee3e391c7d89b123086298011269e1ca5adb6fd
Keshav <keshav0799@gmail.com>
2024-01-07 20:50:22 +0400

    Add Loading Spinner to searchResultsPage

--- client/src/components/searchResultsGrid.jsx
+++ client/src/components/searchResultsGrid.jsx
@@ -3,9 +3,11 @@ import { getSearchResults } from "../apicalls/getSearchResults";
 import { useEffect, useState } from "react";
 import { useSelector} from 'react-redux'
 import { Link } from 'react-router-dom';
+import LoadingSpinner from './loadingSpinner';
 
 export default function SearchResultsGrid({textSearched}){
-    
+
+    const [isLoading, setIsLoading] = useState(false);
     const filter = useSelector((state) => state.filter.filter);
    
 
@@ -17,11 +19,14 @@ export default function SearchResultsGrid({textSearched}){
         useEffect(()=>{
             const fetchData =  async ()=>{
                 try {
+                    setIsLoading(true);
                     const response = await getSearchResults(textSearched,filter);
                     setArray(response.data);
+                    setIsLoading(false);
                    
                 } catch (error) {
                     console.log(error);
+                    setIsLoading(false);
                 }
                
             };
@@ -35,36 +40,37 @@ export default function SearchResultsGrid({textSearched}){
     return(
         <>
 
-        < div className="ProductsGrid">
+        {isLoading?<LoadingSpinner/>:( < div className="ProductsGrid">
 
 
        
-                {myarray?(myarray.map((item,index)=>(
+        {myarray?(myarray.map((item,index)=>(
 
-                <Link to={`/products/${item._id}`}   key={item._id}>
-                    <div className="product">
-                        <div className="picture">
-                    <img src= {item.images[0]} alt="" />
-                </div>
-
-                <div className="info" >
-                    
-                    <div className="productName">{item.name.toUpperCase().substring(0,30)}  </div>
-                    <div className="productPrice">${item.price}</div>
-                </div>
-                    </div>
-                
-                </Link>
-                       
-
-                ))):(<h3>Please try another keyword</h3>)
-                
-                
-                }
-                
+        <Link to={`/products/${item._id}`}   key={item._id}>
+            <div className="product">
+                <div className="picture">
+            <img src= {item.images[0]} alt="" />
         </div>
 
-        
+        <div className="info" >
+            
+            <div className="productName">{item.name.toUpperCase().substring(0,30)}  </div>
+            <div className="productPrice">${item.price}</div>
+        </div>
+            </div>
+
+        </Link>
+            
+
+        ))):("")
+
+
+        }
+
+        </div>
+
+        )}
+       
         
         </>
     )
--- client/src/pages/searchResultsPage.jsx
+++ client/src/pages/searchResultsPage.jsx
@@ -6,16 +6,19 @@ import DisplaySearchResults from '../components/displaySearchResults'
 import { getSearchResults } from '../apicalls/getSearchResults'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
+import LoadingSpinner from '../components/loadingSpinner'
 
 export default function SearchResultsPage(){
     const {searchQuery} =  useParams();
     const [numberOfProducts, setNumberOfProducts] = useState(0);
+    const [isLoading, setIsLoading] = useState(false);
     
     useEffect(()=>{
 
         const fetchData = async() =>{
+
             try {
-                
+                setIsLoading(true);
                 const results = await getSearchResults(searchQuery);
                 if(results.data.length>0){
                     setNumberOfProducts(results.data.length);
@@ -24,9 +27,11 @@ export default function SearchResultsPage(){
                 else{
                     setNumberOfProducts(0);
                 }
+                setIsLoading(false);
                 
             } catch (error) {
                 console.log(error);
+                setIsLoading(false);
             }
 
         };
@@ -44,7 +49,7 @@ export default function SearchResultsPage(){
 
         <NavBar/>
 
-        <DisplaySearchResults numberOfProductsFound={numberOfProducts}  querydata ={searchQuery.toUpperCase()}/>
+        {isLoading?<LoadingSpinner/>:(<DisplaySearchResults numberOfProductsFound={numberOfProducts}  querydata ={searchQuery.toUpperCase()}/>)}
 
 
         <Footer/>
