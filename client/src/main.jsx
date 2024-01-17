9d3b694ce561ab4c46fb653971d23affedc58d7f
Keshav <keshav0799@gmail.com>
2024-01-17 20:39:16 +0400

    Format index.css to add all media queries at the end of the file and remove duplicates

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,29 +1,20 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import Router from './Routes.jsx'
-
 import { store } from './redux/store.jsx'
 import { Provider } from 'react-redux'
 import ToastProvider from './components/toastProvider.jsx'
 
-
-
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
+ 
   <Provider store={store}>
-    <ToastProvider>
+  <ToastProvider>
+
     <Router/>
-    </ToastProvider>
+
+  </ToastProvider>
   
-    
-   
+  </Provider>
    
   
-     </Provider>
-   
-  // {/* </React.StrictMode>, */}
 )
