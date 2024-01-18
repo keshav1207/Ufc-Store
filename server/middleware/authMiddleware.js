8d77b5648efd6a730ea7755796f492ec8b6b5710
Keshav <keshav0799@gmail.com>
2024-01-18 17:12:57 +0400

    Rewrite authMiddleware in more concise and clean code

--- server/middleware/authMiddleware.js
+++ server/middleware/authMiddleware.js
@@ -1,38 +1,35 @@
 const jwt = require("jsonwebtoken");
-const asyncHandler = require("express-async-handler");
 require('dotenv').config();
 
 
-const authMiddleware = asyncHandler(async(req,res,next)=>{
+const authMiddleware = async(req,res,next)=>{
     
+    const authorizationHeader = req.header("authorization");
+
+    if (!authorizationHeader) {
+    return res.status(401).json({ error: "Authorization header missing" });
+    }
     
-    const token = req.header("authorization").split(" ")[1];
+    const token = authorizationHeader.split(" ")[1];
    
    
-   
-    
-    
-    
     try {
-        const decryptedToken = jwt.verify(token,process.env.SECRET_KEY);
-       
-    
+
+        const decryptedToken = jwt.verify(token,process.env.SECRET_KEY,{ algorithms: ["HS256"] });
         req.body.userId = decryptedToken.userId;
-       
-        next() ;  
+        next() ; 
+
     } catch (error) {
-        console.log(error.message);
+
+        console.error("JWT verification error:", error);
+
         if (error.message ==="jwt expired") {
             return res.status(401).json({ error: "Token expired" });
         } else {
-            
             return res.status(500).json({ error: "Internal Server Error" });
         }
     }
-    
-
-    
-    
-})
+       
+}
 
 module.exports = authMiddleware;
