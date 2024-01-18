bfcf726bcbe3e5532c3f0581d08fb07b51b2f2e0
Keshav <keshav0799@gmail.com>
2024-01-18 16:55:31 +0400

    Add try-catch block to our config files

--- server/config/connect.js
+++ server/config/connect.js
@@ -1,7 +1,14 @@
 const mongoose = require('mongoose')
 
-const connectDB = (url) => {
-  return mongoose.connect(url)
+const connectDB = async (url) => {
+  try {
+    await mongoose.connect(url);
+    console.log('Connected to the database');
+  } catch (error) {
+    console.error('Error connecting to the database:', error.message);
+    process.exit(1); // Exit with failure code, process.exit is used for graceful termination on failure
+  }
+  
 }
 
 module.exports = connectDB
