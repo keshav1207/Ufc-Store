10638f5ac297ff942cf90e9693a5663074c7805a
Keshav <keshav0799@gmail.com>
2024-01-20 21:31:05 +0400

    Add comment to getNewProductsRoute

--- server/routes/getNewProductsRoute.js
+++ server/routes/getNewProductsRoute.js
@@ -7,7 +7,7 @@ const Product = require("../models/productModel");
 //Get  newest 3 products from database
 router.get('/',asyncHandler( async function(req, res, next) {
 
-    
+    //Sort most recent using createdAT and then the top 3 results using the limit property.
     const newProducts = await Product.find().sort({ createdAt: -1 }).limit(3).exec();
 
 
