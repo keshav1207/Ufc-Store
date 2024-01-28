23d9d20b1224ae9e0e06d3903514c496faa0b609
Keshav <keshav0799@gmail.com>
2024-01-28 18:41:17 +0400

    Create apiService.js file

--- client/src/apicalls/getNewProducts.js
+++ client/src/apicalls/getNewProducts.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import { API_BASE_URL } from "./apiService";
 
 export const getNewProducts = async function(){
     try {
        
-        const response = await axios.get("http://localhost:5000/api/newProducts");
+        const response = await axios.get(`${API_BASE_URL}/api/newProducts`);
         
         return response.data;
         
